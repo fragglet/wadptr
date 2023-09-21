ce21e8c012dc121c251bf2c31d0290d3d871770d
Simon Howard <fraggle@soulsphere.org>
2023-09-20 23:26:13 -0400

    Update copyright year.

--- wadmerge.c
+++ wadmerge.c
@@ -1,6 +1,6 @@
 /**************************************************************************
  *                                                                        *
- * Copyright(C) 1998-2011 Simon Howard, Andreas Dehmel                    *
+ * Copyright(C) 1998-2023 Simon Howard, Andreas Dehmel                    *
  *                                                                        *
  * This program is free software; you can redistribute it and/or modify   *
  * it under the terms of the GNU General Public License as published by   *
