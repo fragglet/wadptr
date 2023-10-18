fragglet/wadptr

commit c2a58e0577076c9aba4c0d1d6c30eb45fbff78a3

Move Hexen format detection to sidedefs.c.
This is only used in the sidedefs code anyway.

sidedefs.c
@@ -128,6 +128,7 @@ static int linedefnum; /* linedef wad entry number */
 
 static linedef_array_t linedefs_result;
 static sidedef_array_t sidedefs_result;
+static bool hexen_format;
 
 // Maps old sidedef number to new; used by RemapLinedefs() to update the
 // sidedef references in the LINEDEFS lump.
@@ -135,7 +136,7 @@ static sidedef_ref_t *newsidedef_index;
 
 static size_t LinedefSize(void)
 {
-    if (hexen_format_wad)
+    if (hexen_format)
     {
         return HX_LDEF_SIZE;
     }
@@ -144,7 +145,12 @@ static size_t LinedefSize(void)
 
 static linedef_array_t ReadLinedefs(wad_file_t *wf, int lumpnum)
 {
-    if (hexen_format_wad)
+    // Hexen levels have a slightly different format, and we can detect
+    // this by looking for the presence of a BEHAVIOR lump, which is
+    // unique to this format.
+    hexen_format = EntryExists(wf, "BEHAVIOR") >= 0;
+
+    if (hexen_format)
     {
         return ReadHexenLinedefs(wf, lumpnum);
     }
@@ -205,7 +211,7 @@ void P_WriteLinedefs(FILE *fstream, entry_t *entry)
     entry->offset = WriteWadLump(fstream, NULL, 0);
     entry->length = linedefs_result.len * LinedefSize();
 
-    if (hexen_format_wad)
+    if (hexen_format)
     {
         WriteHexenLinedefs(&linedefs_result, fstream);
     }

main.c
@@ -49,7 +49,6 @@ bool allowpack = true;   // level packing on
 bool allowsquash = true; // picture squashing on
 bool allowmerge = true;  // lump merging on
 bool allowstack = true;  // blockmap stacking
-bool hexen_format_wad;
 static bool quiet_mode = false;
 
 static bool FileExists(const char *filename)
@@ -304,13 +303,6 @@ static void Help(void)
         "\n");
 }
 
-// Hexen levels have a slightly different format, and we can detect this by
-// looking for the presence of a BEHAVIOR lump, which is unique to this format.
-static void CheckHexenFormat(wad_file_t *wf, const char *filename)
-{
-    hexen_format_wad = EntryExists(wf, "BEHAVIOR") >= 0;
-}
-
 // LINEDEFS and SIDEDEFS lumps follow each other in Doom WADs. This is
 // baked into the engine - Doom doesn't actually even look at the names.
 static bool IsSidedefs(wad_file_t *wf, int count)
@@ -337,7 +329,6 @@ static bool Compress(const char *wadname)
     {
         return false;
     }
-    CheckHexenFormat(&wf, wadname);
 
     orig_size = FileSize(wf.fp);
 
@@ -519,7 +510,6 @@ static bool Uncompress(const char *wadname)
     {
         return false;
     }
-    CheckHexenFormat(&wf, wadname);
 
     fstream = OpenTempFile(wadname, &tempwad_name);
 
@@ -708,7 +698,6 @@ static bool ListEntries(const char *wadname)
     {
         return false;
     }
-    CheckHexenFormat(&wf, wadname);
 
     SPAMMY_PRINTF(
         " Number  Length  Offset      Method      Name        Shared\n"

wadptr.h
@@ -24,7 +24,6 @@
 extern bool allowpack;   // level packing on
 extern bool allowsquash; // picture squashing on
 extern bool allowmerge;  // lump merging on
-extern bool hexen_format_wad;
 
 #ifdef _WIN32
 #define DIRSEP "\\"

commit 355bb56f6c72c693acada7d6a36eae9a569356d9

Oops.
commit c2a58e0577076c9 broke Hexen map support because the
hexen_format variable wasn't being initialized at the right time.
While we're fixing this, add CheckLumpSizes() call to P_IsPacked()
as well.

sidedefs.c
@@ -145,11 +145,6 @@ static size_t LinedefSize(void)
 
 static linedef_array_t ReadLinedefs(wad_file_t *wf, int lumpnum)
 {
-    // Hexen levels have a slightly different format, and we can detect
-    // this by looking for the presence of a BEHAVIOR lump, which is
-    // unique to this format.
-    hexen_format = EntryExists(wf, "BEHAVIOR") >= 0;
-
     if (hexen_format)
     {
         return ReadHexenLinedefs(wf, lumpnum);
@@ -291,6 +286,8 @@ bool P_IsPacked(wad_file_t *wf, int sidedef_num)
     // SIDEDEFS always follows LINEDEFS.
     linedefnum = sidedef_num - 1;
     sidedefnum = sidedef_num;
+
+    CheckLumpSizes(wf);
 
     linedefs = ReadLinedefs(wf, linedefnum);
 
@@ -476,7 +473,14 @@ static void RemapLinedefs(linedef_array_t *linedefs)
 
 static void CheckLumpSizes(wad_file_t *wf)
 {
-    unsigned int linedef_size = LinedefSize();
+    unsigned int linedef_size;
+
+    // Hexen levels have a slightly different format, and we can detect
+    // this by looking for the presence of a BEHAVIOR lump, which is
+    // unique to this format.
+    hexen_format = EntryExists(wf, "BEHAVIOR") >= 0;
+    linedef_size = LinedefSize();
+
     if ((wf->entries[linedefnum].length % linedef_size) != 0)
     {
         ErrorExit("RebuildSidedefs: LINEDEFS lump (#%d) is %d bytes, "